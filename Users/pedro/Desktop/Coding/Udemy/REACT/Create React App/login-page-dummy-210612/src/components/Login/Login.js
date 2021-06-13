500e91777d6fc6a549bc7941586250d793ba4b9b
Pedro Batista <pedromachakio@hotmail.com>
2021-06-13 17:21:43 +0000

    changed a couple validations to use the reducer's object value instead

--- Users/pedro/Desktop/Coding/Udemy/REACT/Create React App/login-page-dummy-210612/src/components/Login/Login.js
+++ Users/pedro/Desktop/Coding/Udemy/REACT/Create React App/login-page-dummy-210612/src/components/Login/Login.js
@@ -56,15 +56,13 @@ const Login = (props) => {
   const emailChangeHandler = (event) => {
     dispatchEmail({ type: "EMAIL_USER_INPUT", val: event.target.value }); // action
 
-    setFormIsValid(
-      event.target.value.includes("@") && passwordState.value.trim().length > 6
-    );
+    setFormIsValid(event.target.value.includes("@") && passwordState.isValid);
   };
 
   const passwordChangeHandler = (event) => {
     dispatchPassword({ type: "PASSWORD_USER_INPUT", val: event.target.value }); // action
 
-    setFormIsValid(emailState.isValid && event.target.value.trim().length > 6);
+    setFormIsValid(emailState.isValid && passwordState.isValid);
   };
 
   const validateEmailHandler = () => {
